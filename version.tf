b2754d928182486be27fc6ce14411a23e49b2465
pijain <d3vb0xx@gmail.com>
2024-06-18 18:14:29 +0530

    Initial commit

---
+++ version.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_version = "~> 1.8"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2"
+    }
+  }
+}
